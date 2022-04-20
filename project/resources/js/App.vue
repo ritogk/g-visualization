d3a379a6ae648920c305765a9e7d23b3a01ee597
root <homing-job@yahoo.co.jp>
2022-04-20 17:58:13 +0900

    spaが動く所まで

---
+++ project/resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <transition name="fade">
+      <router-view />
+    </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    setup(){
+        return {}
+    }
+})
+</script>
