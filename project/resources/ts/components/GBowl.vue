6711677dba36d5a01e4e049e2b9aca6fd89244e9
root <homing-job@yahoo.co.jp>
2022-05-02 10:29:02 +0900

    gbowのデザイン追加

--- project/resources/ts/components/GBowl.vue
+++ project/resources/ts/components/GBowl.vue
@@ -1,5 +1,5 @@
 <template>
-  <p></p>
+  <div id="p5Canvas"></div>
 </template>
 
 <script lang="ts">
@@ -39,7 +39,9 @@ export default defineComponent({
 
     const sketch = (p: p5) => {
       p.setup = () => {
-        p.createCanvas(canvas_width, canvas_height)
+        const canvas = p.createCanvas(canvas_width, canvas_height)
+        canvas.parent('p5Canvas')
+        p.background(43, 53, 63)
         p.angleMode('degrees')
         p.frameRate(60)
         p.translate(canvas_width / 2, canvas_height / 2)
@@ -64,10 +66,10 @@ export default defineComponent({
           )
 
           // x軸とy軸のgの和を表示
-          p.fill(255)
+          p.fill(43, 53, 63)
           p.noStroke()
           p.rect(canvas_width / 2 - 32, (canvas_height / 2) * -1 + 3, 30, 18)
-          p.fill(0)
+          p.fill(158, 158, 147)
           const g_xy =
             Math.ceil(Math.sqrt(props.x ** 2 + props.y ** 2) * 10) / 10
           p.text(
@@ -89,16 +91,19 @@ export default defineComponent({
       }
 
       const initCanvas = () => {
-        p.fill(255)
-        p.stroke(0, 0, 0)
-        p.strokeWeight(1)
         // キャンバスの枠を描画
+        p.fill(43, 53, 63)
+        p.noStroke()
+        p.strokeWeight(1)
         p.rect(
           -canvas_width / 2,
           -canvas_height / 2,
           canvas_width,
           canvas_height
         )
+
+        p.noFill()
+        p.stroke(158, 158, 147)
         // gサークルを描画
         p.ellipse(0, 0, g_circle_diameter)
         const num = max_g * 10
@@ -109,25 +114,23 @@ export default defineComponent({
         p.ellipse(0, 0, (g_circle_diameter * 4) / num)
         p.ellipse(0, 0, (g_circle_diameter * 2) / num)
 
-        //const diff_canvas_cicle = (canvas_height - g_circle_diameter) / 2
-
         // gサークルの横線を描画
         p.line((g_circle_diameter / 2) * -1, 0, g_circle_diameter / 2, 0)
         // gサークルの縦線を描画
         p.line(0, (g_circle_diameter / 2) * -1, 0, g_circle_diameter / 2)
 
         // ラベルを描画
-        p.fill(0)
+        p.fill(158, 158, 147)
         p.strokeWeight(0)
         const cicle_interval = g_circle_diameter / 2 / 7
 
-        p.text('1.4', 0, cicle_interval * 7 * -1 + 5)
-        p.text('1.2', 0, cicle_interval * 6 * -1 + 5)
-        p.text('1.0', 0, cicle_interval * 5 * -1 + 5)
-        p.text('0.8', 0, cicle_interval * 4 * -1 + 5)
-        p.text('0.6', 0, cicle_interval * 3 * -1 + 5)
-        p.text('0.4', 0, cicle_interval * 2 * -1 + 5)
-        p.text('0.2', 0, cicle_interval * -1 + 5)
+        p.text('1.4G', 0, cicle_interval * 7 * -1 + 5)
+        p.text('1.2G', 0, cicle_interval * 6 * -1 + 5)
+        p.text('1.0G', 0, cicle_interval * 5 * -1 + 5)
+        p.text('0.8G', 0, cicle_interval * 4 * -1 + 5)
+        p.text('0.6G', 0, cicle_interval * 3 * -1 + 5)
+        p.text('0.4G', 0, cicle_interval * 2 * -1 + 5)
+        p.text('0.2G', 0, cicle_interval * -1 + 5)
 
         p.text('前', 0 - 20, (canvas_height / 2) * -1 + 13)
         p.text('後', 0 - 20, canvas_height / 2 - 4)
@@ -135,7 +138,7 @@ export default defineComponent({
         p.text('左', (canvas_width / 2) * -1 + 1, -10)
 
         // 時間軸を描画
-        p.stroke(0, 0, 0)
+        p.stroke(158, 158, 147)
         p.strokeWeight(1)
         for (let i = 0; i < 12; i++) {
           p.rotate(30)
@@ -146,9 +149,8 @@ export default defineComponent({
             0,
             -(g_circle_diameter / 2) + 2
           )
-          p.fill(0)
-          p.strokeWeight(0)
-          p.text(i + 1, -4, -(g_circle_diameter / 2) - 6)
+          //p.strokeWeight(0)
+          //p.text(i + 1, -4, -(g_circle_diameter / 2) - 6)
         }
       }
     }
