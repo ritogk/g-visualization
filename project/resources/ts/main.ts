50f1381af2121e1bf59b09afbeb100248419ee9c
root <homing-job@yahoo.co.jp>
2022-05-04 15:42:41 +0900

    こまごまとした修正

--- project/resources/ts/main.ts
+++ project/resources/ts/main.ts
@@ -8,6 +8,17 @@ import router from './route'
 
 import 'bootstrap'
 
+/* ピッチインピッチアウトによる拡大縮小を禁止 */
+document.documentElement.addEventListener(
+  'touchstart',
+  function (e) {
+    if (e.touches.length >= 2) {
+      e.preventDefault()
+    }
+  },
+  { passive: false }
+)
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
