2089cd02f01f40347ca76392dfa7aa6259e3d04a
root <homing-job@yahoo.co.jp>
2022-05-04 16:02:10 +0900

    googleanaliticys タグついあk

--- project/resources/views/app.blade.php
+++ project/resources/views/app.blade.php
@@ -22,6 +22,16 @@
 
     <!-- ファビコン -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160164762-2"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-160164762-2');
+    </script>
+
 </head>
 
 <body>
