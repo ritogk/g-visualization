935663119e9b24b9341bb3194e9c97b4a236b464
root <homing-job@yahoo.co.jp>
2022-05-10 18:08:02 +0900

    多言語回りの修正

--- project/routes/web.php
+++ project/routes/web.php
@@ -6,10 +6,9 @@ use App\Http\Controllers;
 Route::get('/ja/lp', [Controllers\LPController::class, 'show_ja'])->name('ja.lp');
 Route::get('/en/lp', [Controllers\LPController::class, 'show_en'])->name('en.lp');
 
-
 // spaのルートを「/」から「/app/*」に変えたのでリダイレクトを挟む。
 Route::get('/', function () {
-    return redirect('/app/g-analys');
+    return redirect('/app/index?lang=ja');
 });
 
 // spaのルーティング
