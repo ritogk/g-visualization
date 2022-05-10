b7650b37ab34fcfc20153e4dd6f648019530e8de
root <homing-job@yahoo.co.jp>
2022-05-10 14:04:51 +0900

    lpの多言語対応

---
+++ project/resources/lang/en/lp.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'スポーツ走行に特化したGを可視化するアプリ' => 'G visualization application specialized for sports driving',
+    '簡単操作でGの可視化が行なえます。ドライビングモニターとしてもご利用いただけます。' => 'G visualization with easy operation. It can also be used as a driving monitor.',
+
+    'Gを....操れ....!!' => 'G is .... Manipulate ....!!!',
+
+    '完全無料' => 'Completely free',
+    'すべてのサービスを完全無料でご利用いただけます。' => 'All services are completely free of charge.',
+    'ｽﾎﾟｰﾂ走行に特化' => 'Specialized for sports driving',
+    'スポーツ走行向けに様々なチューニングを行っております。' => 'We offer various tuning services for sports driving.',
+    '豊富なテーマ' => 'Extensive themes',
+    'Gを可視化するためのテーマを複数ご用意しています。' => 'Multiple themes available for visualizing G',
+    'iphone対応' => 'iphone support',
+    'safari, chromeで動作確認済。androidは後日対応します。' => 'Safari and chrome tested. android will be supported at a later date.', 'iphone support' => 'iphone support', 'safari and chrome tested. android will be supported at a later date.',
+    '簡単操作' => 'Easy operation',
+    '複雑な操作はありません。' => 'No complicated operation.',
+
+    '基本機能' => 'Basic functions',
+    'Gセンサー値の可視化(インジケータータイプ)' => 'Visualization of G sensor value (indicator type)',
+    'Gセンサー値の可視化(ボールタイプ)' => 'Visualization of G Sensor Value (Ball Type)',
+    'Gセンサーの感度調整(路面、足回りの硬さに合わせて調整して下さい。)' => 'G Sensor Sensitivity Adjustment (Adjust according to the road surface and suspension hardness.)',
+    '対応機種(iphone)' => 'Compatible model(iphone)',
+
+    'デモ' => 'demo',
+
+    'iPhone対応、androidは近日対応予定' => 'iPhone support, android support coming soon',
+
+    '今すぐ起動' => 'Launch Now',
+    'お問い合わせ' => 'Contact us',
+];
