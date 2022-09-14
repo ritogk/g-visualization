d2774983124452799adaf2b9c830440c5cf698ca
r.ito <>
2022-09-14 13:47:35 +0900

    seiri

--- project/resources/ts/libs/worker/vectorRotate.ts
+++ project/resources/ts/libs/worker/vectorRotate.ts
@@ -1,10 +1,22 @@
-import { rotate3dVector }  from '@/libs/trigonometric'
+/**
+ * 三次元ベクトルの回転処理
+ * ※worker用
+ */
+
+import { rotate3dVector } from '@/libs/trigonometric'
 
 const w: Worker = self as any
 
 w.onmessage = (event: MessageEvent) => {
-  const data : any = event.data
-  const rotate_acceration = rotate3dVector(data.x, data.y, data.z, data.angle_x, data.angle_y, data.angle_z)
+  const data: any = event.data
+  const rotate_acceration = rotate3dVector(
+    data.x,
+    data.y,
+    data.z,
+    data.angle_x,
+    data.angle_y,
+    data.angle_z
+  )
   w.postMessage(rotate_acceration)
 }
 
