dd8811f8747f5e3c3cd9e0fcbdad6294aaad8a0e
r.ito <>
2022-09-21 16:48:31 +0900

    code kireini sita yo

--- project/openapi/generator_ts.sh
+++ project/openapi/generator_ts.sh
@@ -3,7 +3,7 @@ WORK_PATH='/out/ts'
 # コード生成
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v5.3.0 generate -i /local/api.yaml -g typescript-fetch -o /local${WORK_PATH} -c /local/config_ts.json
 
-OUTPUT_PATH='../resources/ts/openapi'
+OUTPUT_PATH='../resources/ts/core/openapiClient'
 # vue側にコピー
 rm -rf ${OUTPUT_PATH}
 mkdir -p ${OUTPUT_PATH}
